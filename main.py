c79bfd3a33664855abbc9389760fbe4eaa5aed7f
dogb29022-ux <dogb29022@gmail.com>
2026-02-11 14:32:58 +0545

    add function named handle_execution

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ import gui_components
 import execution_logic
 import os
 
-def main():
-    pass
+def handle_execution(input_widget, output_widget):
+    """
+    The bridge function that gets text from the GUI and sends it to the logic.
+    """
+    # 1. Get code from the UI
+    user_code = input_widget.get("1.0", tk.END)
