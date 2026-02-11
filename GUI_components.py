c9b7cffdc704e1fe13db23e301b3638707284ebf
dogb29022-ux <dogb29022@gmail.com>
2026-02-11 14:31:19 +0545

    add header lebel in  github

--- GUI_components.py
+++ GUI_components.py
@@ -2,4 +2,9 @@ import tkinter as tk
 from tkinter import scrolledtext
 
 def create_widgets(root, run_callback):
-    
+    """
+    Creates the UI elements and places them in the window.
+    """
+    # Header Label
+    header = tk.Label(root, text="Python Code Injector", font=("Arial", 16, "bold"))
+    header.pack(pady=10)
