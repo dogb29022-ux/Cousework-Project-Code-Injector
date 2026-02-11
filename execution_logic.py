a419a446e2175e45e68d483fb645a6fa51defcdd
dogb29022-ux <dogb29022@gmail.com>
2026-02-11 14:29:04 +0545

    added for loop

--- execution_logic.py
+++ execution_logic.py
@@ -1,2 +1,10 @@
 import subprocess
 import os
+
+def recursive_cleanup(directory, pattern="temp_script_"):
+    """
+    A recursive function that searches a directory and deletes 
+    any files starting with the specified pattern.
+    """
+    for item in os.listdir(directory):
+        path = os.path.join(directory, item)
