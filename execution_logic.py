dc1b82516cdc3acc2f936ef4523114ba37438834
dogb29022-ux <dogb29022@gmail.com>
2026-02-11 14:29:58 +0545

    add if else in github

--- execution_logic.py
+++ execution_logic.py
@@ -8,3 +8,10 @@ def recursive_cleanup(directory, pattern="temp_script_"):
     """
     for item in os.listdir(directory):
         path = os.path.join(directory, item)
+
+        # If it's a directory, recurse into it
+        if os.path.isdir(path):
+            recursive_cleanup(path, pattern)
+        # If it's a file and matches our temp pattern, delete it
+        elif item.startswith(pattern) and item.endswith(".py"):
+            os.remove(path)
